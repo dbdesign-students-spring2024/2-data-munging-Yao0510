396127cf1029eb17aae9395705d4e933fc6de7a4
Yao0510 <yw4822@nyu.edu>
2024-02-09 20:41:12 -0500

    finish assignment

--- analyze.py
+++ analyze.py
@@ -1,1 +1,24 @@
-# Place code below to do the analysis part of the assignment.
+import csv
+
+
+def calculate_decadal_averages(file_path):
+    decadal_averages = {}
+    with open(file_path, 'r') as file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            year = int(row['Year'])
+            anomaly_fahrenheit = float(row['J-D'])
+            decade = (year // 10) * 10
+            if decade in decadal_averages:
+                decadal_averages[decade].append(anomaly_fahrenheit)
+            else:
+                decadal_averages[decade] = [anomaly_fahrenheit]
+
+    for decade, anomalies in decadal_averages.items():
+        average_anomaly = sum(anomalies) / len(anomalies)
+        print(f'{decade}s: {average_anomaly:.2f} F')
+
+
+if __name__ == '__main__':
+    file_path = 'data/clean_data.csv'
+    calculate_decadal_averages(file_path)
--- munge.py
+++ munge.py
@@ -1,1 +1,49 @@
 # Place code below to do the munging part of this assignment.
+original_data_filename = 'data/GLB.Ts+dSST.txt'
+munged_data_filename = 'data/clean_data.csv'
+heading_string = 'Year   Jan  Feb  Mar  Apr  May  Jun  Jul  Aug  Sep  Oct  Nov  Dec    J-D D-N    DJF  MAM  JJA  SON  Year'
+
+
+def celcius_to_farenheit_diff(celcius):
+    return celcius * 9/5
+
+def readlines(filename):
+    with open(filename) as f:
+        return f.readlines()
+    
+def clean_data(lines):
+    cleaned_lines = []
+    for line in lines:
+        try :
+            int(line.split()[0])
+            cleaned_lines.append(line)
+        except ValueError:
+            pass
+        except IndexError:
+            pass
+    return cleaned_lines
+
+def write_to_file(filename, lines):
+    headings = heading_string.split()[:-1]
+    heading_csv = ','.join(headings)
+    with open(filename, 'w') as f:
+        f.write(heading_csv + '\n')
+        for line in lines:
+            raw_data = line.split()[:-1]
+            for i in range(1, len(raw_data)):
+                try:
+                    float(raw_data[i])
+                except ValueError:
+                    raw_data[i] = 'NaN'
+                farenheit = celcius_to_farenheit_diff(float(raw_data[i])/100)
+                raw_data[i] = format(farenheit, '.1f')
+            f.write(','.join(raw_data) + '\n')
+
+
+if __name__ == '__main__':
+    lines = readlines(original_data_filename)
+    cleaned_lines = clean_data(lines)
+    write_to_file(munged_data_filename, cleaned_lines)
+    print('Data munged and written to file')
+
+        
